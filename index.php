8495ccd03978bf897e5a27476885c11b011b8c6e
thinhcubi <thinhcubi27@gmail.com>
2021-05-10 14:00:34 +0700

    index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+class Application
+{
+    private static Application $instance;
+    private function __construct()
+    {
+    }
+
+    public static function getInstance(): Application
+    {
+        if (self::$instance === null) {
+            self::$instance = new Application();
+        }
+        return self::$instance;
+    }
+}
+$app1 = Application::getInstance();
+$app2 = Application::getInstance();
+$app3 = Application::getInstance();
